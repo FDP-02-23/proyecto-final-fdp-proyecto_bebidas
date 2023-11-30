620bb09bd9f1e25dd56fc5c8d2d036ac300e1762
Valency012 <00090123@uca.edu.sv>
2023-11-30 15:08:58 -0600

    Agregar declaracion de Funcion Fecha

--- funciones.cpp
+++ funciones.cpp
@@ -5,4 +5,13 @@
 #include <ctime>
 #include <string>
 
+//Definicion de Funciones
+
+string GetCurrentDate()
+{
+    time_t t = time(0);
+    tm *now = localtime(&t);
+    return to_string(now->tm_mday) + "/" + to_string(now->tm_mon + 1) + "/" + to_string(now->tm_year + 1900);
+}
+
 //Archivo que se encarga de la ejecucion de funciones
