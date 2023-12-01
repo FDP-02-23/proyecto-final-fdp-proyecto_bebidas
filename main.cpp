de5bbea71f8c7f040e286aad5d8e41825f9b0838
Romero39 <128502097+Romero39@users.noreply.github.com>
2023-12-01 02:46:23 -0600

    Opciones jefe administrador completadas

--- main.cpp
+++ main.cpp
@@ -15,7 +15,10 @@ int main()
 
     Bebida bebida;           // Declaracion estructura
     Usuarios usuarios;       // Declaracion estructura
-    Ventas ventas;           // Declaracion estructura
+    Ventas ventas; 
+    Bitacora_usu bitacora;   // Declaracion estructura
+    Bitacora_prod bitacoraprod;
+    Bitacora_ven bitacoraven;          // Declaracion estructura
     int opcion;              // Variable encargada de evaluar la opcion ingresada por el cliente
     string nombre_usuario;   // Variable que captura el nombre de usuario
     string contrasenia;      // Variable que captura la clave
@@ -30,25 +33,28 @@ int main()
     int lote = 0;            // Variable que sirve como identificador cada producto, generada automaticamente
     int identificador = 0;   // Variable que sirve como identificador unico cada venta
 
-    // Insertar datos vector usuarios, administrador
-    usuario.push_back({"admin123", "123", 1});
-    usuario.push_back({"admin456", "456", 1});
-    usuario.push_back({"admin789", "789", 2});
+    //Funcion para llenar el vector
+    InicializarUsuarios();
 
     do
     {
         system("cls");
         // Encabezado del sistema
         cout << endl;
-        cout << "                ------PEPITO'S------" << endl;
-        cout << "   ------- TIENDA DISTRIBUIDORA DE BEBIDAS -------" << endl;
+        cout << "             ------PEPITO'S------" << endl;
+        cout << "   ------- DISTRIBUIDORA DE BEBIDAS -------" << endl;
         cout << endl;
         cout << "        En vez de una, llevate varias" << endl;
         cout << endl;
+
         cout << "*************************************************" << endl;
-        
-         // Llamamiento a la funcion de buscar usuario, encargada de verificar si es admin o empleado
-        tipo_usuario = BuscarUsuario();
+        cout << "Ingrese su usuario: ";
+        cin >> nombre_usuario;
+        cout << "Ingrese su clave: ";
+        cin >> contrasenia;
+        cin.ignore();
+        cout << endl;
+        tipo_usuario = BuscarUsuario(nombre_usuario, contrasenia);
 
         do
         { // Do while el funcionamiento de regresar a menus anteriores
@@ -82,6 +88,104 @@ int main()
                     case 1: 
                     cout << "------- MANEJO DE USUARIOS ------- " << endl;
                     cout << endl;
+                    do
+                        {
+                        switch (opcion1)
+                            {
+                            case 1:
+                                cout << " Ingrese el nombre de usuario: ";
+                                cin >> usuarios.usuario;
+                                cout << " Ingrese una clave: ";
+                                cin >> usuarios.contrasenia;
+                                cout << endl;
+                                cout << " Ingrese el tipo de usuario:\n (1) Si es administrador\n (2) Si es empleado\n >";
+                                cin >> usuarios.tipo_usuario;
+                                cout << endl;
+                                usuarios.usuario_creacion = nombre_usuario;
+
+                                if (usuarios.tipo_usuario != 1 && usuarios.tipo_usuario != 2)
+                                {
+                                    cout << "Tipo de usuario no valido!" << endl;
+                                    regresarmenuusu = 1;
+                                    regresar = 0;
+                                }
+                                else
+                                {
+                                    AgregarUsuarios(usuarios);
+                                    //Añadiendo el registro a la estructura de bitacora y asi llevar registro
+                                    bitacora.usuario = nombre_usuario;
+                                    bitacora.usuarios.usuario = usuarios.usuario;
+                                    bitacora.usuarios.contrasenia = usuarios.contrasenia;
+                                    bitacora.usuarios.tipo_usuario = usuarios.tipo_usuario;
+                                    bitacora.usuario_afectado = usuarios.usuario;
+                                    bitacora.usuarios.usuario_creacion = nombre_usuario;
+                                    bitacora.accion = "Se agrego un usuario";
+                                    bitacora.fecha = GetCurrentDate();
+                                    AgregarBitacoraUsu(bitacora);
+                                    regresarmenuusu = 1;
+                                    regresar = 0;
+                                }
+                                break;
+                            case 2:
+                                ImprimirUsuarios();
+                                regresarmenuusu = 1;
+                                regresar = 0;
+                                break;
+                            case 3:
+                                ImprimirUsuarios();
+                                cout << " Ingrese el nombre del usuario a modificar: ";
+                                cin >> usuarios.usuario;
+                                //Añadiendo el registro a la estructura de bitacora y asi llevar registro
+                                bitacora.usuario = nombre_usuario;
+                                bitacora.usuarios.usuario = usuarios.usuario;
+                                bitacora.usuarios.contrasenia = usuarios.contrasenia;
+                                bitacora.usuarios.tipo_usuario = usuarios.tipo_usuario;
+                                bitacora.usuario_afectado = usuarios.usuario;
+                                bitacora.usuarios.usuario_creacion = nombre_usuario;
+                                bitacora.accion = "Se modifico un usuario";
+                                bitacora.fecha = GetCurrentDate();
+
+                                ModificarUsuario(usuarios.usuario);
+                                AgregarBitacoraUsu(bitacora);
+
+                                regresarmenuusu = 1;
+                                regresar = 0;
+
+                                break;
+                            case 4:
+                                ImprimirUsuarios();
+                                cout << " Ingrese el nombre de usuario a eliminar: ";
+                                cin >> usuarios.usuario;
+
+                                EliminarUsuario(usuarios.usuario);
+                                cout << endl;
+                                bitacora.usuario = nombre_usuario;
+                                bitacora.usuarios.usuario = usuarios.usuario;
+                                bitacora.usuarios.contrasenia = usuarios.contrasenia;
+                                bitacora.usuarios.tipo_usuario = usuarios.tipo_usuario;
+                                bitacora.usuario_afectado = usuarios.usuario;
+                                bitacora.usuarios.usuario_creacion = nombre_usuario;
+                                bitacora.accion = "Se elimino un usuario";
+                                bitacora.fecha = GetCurrentDate();
+                                AgregarBitacoraUsu(bitacora);
+                                regresarmenuusu = 1;
+                                regresar = 0;
+                                break;
+                            case 5:
+                                regresarmenuusu = 1;
+                                regresar = 1;
+                                contador = 0;
+                                break;
+                            default:
+                                cout << "Opcion no valida! Regresando..." << endl;
+                                cout << endl;
+                                regresarmenuusu = 1;
+                                regresar = 1;
+                                break;
+                            }
+                            system("pause");
+
+                    } while (regresarmenuusu == 0);
                     break; 
 
                     // Opcion 2 de menu Administrador
@@ -120,7 +224,7 @@ int main()
                                 // Verificar si el tipo pack es diferente a 1, 2 o 3, si lo es, requerir un valor valido
                                 if (bebida.tipo_bebida != 1 && bebida.tipo_bebida != 2 && bebida.tipo_bebida != 3)
                                 {
-                                    cout << " Ingrese una opcion valida!\n"
+                                    cout << " Ingrese una opcion valida!\n";
                                     cout << endl;
                                     regresarmenupro = 1;
                                     regresar = 0;
@@ -198,51 +302,68 @@ int main()
 
                     // Opcion 3 del menu Administrador
                     case 3: 
-                    cout << endl; 
-                    cout << "----- REGISTRO DE VENTAS DIARIAS ----- " << endl;
-                    break; 
-
-                    // Opcion 4 del menu Administrador
-                    case 4: 
-                    cout << "---------- BITACORA ---------- " << endl;
+                    if (opcion == 3)
+                    {
+                        cout << endl;
+                        cout << "----- REGISTRO DE VENTAS ----- " << endl;
+                        MostrarRegistro("RegistroVentas.txt"); // Imprime el registro
+                    }
+                    else
+                    {
+                        cout << "El archivo se ha creado aparte" << endl;
+                    }
                     system("pause");
                     regresar = 1;
                     break; 
 
+                    // Opcion 4 del menu Administrador
+                    case 4: 
+                    cout << "Ingrese el nombre de usuario para verificar las acciones realizadas: ";
+                    getline(cin, bitacora.usuario);
+
+                    cout << "---------- BITACORA ---------- " << endl;
+                    ImprimirBitacoraUsu(bitacora.usuario);
+                    ImprimirBitacoraPro(bitacora.usuario);
+                    ImprimirBitacoraVen(bitacora.usuario),
+                    system("pause");
+                    contador = 0;
+                    regresar = 1;
+                    break;
+
                      // Opcion 5 del menu Administrador
                     case 5: 
-                        cout << "De verdad desea cerrar sesion? Si (0), No (1)  \n > ";
-                        cin >> sesion;
-                        cout << endl;
-                        if (sesion == 0)
-                        {
-                            cout << " Ha cerrado sesion" << endl;
-                            contador = 1;
-                            regresar = 1;
-                        }
-                        else if (sesion == 1)
-                        {
-                            cout << "  Sesion manetenida" << endl;
-                            contador = 0;
-                            regresar = 1;
-                        }
-                        else
-                        {
-                            sesion = 0;
-                            cout << "Opcion no valida! Cerrado sesion..." << endl;
-                            contador = 1;
-                            regresar = 1;
-                        }
-                        system("pause");
+                    cout << "De verdad desea cerrar sesion? Si (0), No (1)  \n > ";
+                    cin >> sesion;
+                    cout << endl;
+                    if (sesion == 0)
+                    {
+                        cout << " Ha cerrado sesion" << endl;
+                        contador = 1;
+                        regresar = 1;
+                    }
+                    else if (sesion == 1)
+                    {
+                        cout << "  Sesion manetenida" << endl;
+                        contador = 0;
+                        regresar = 1;
+                    }
+                    else
+                    {
+                        sesion = 0;
+                        cout << "Opcion no valida! Cerrado sesion..." << endl;
+                        contador = 1;
+                        regresar = 1;
+                    }
+                    system("pause");
                     break;
                     system("cls");
 
                     default:
-                        cout << "Ingrese una opcion valida! " << endl;
-                        cout << endl;
-                        regresar = 1;
-                        contador = 0;
-                        break;
+                    cout << "Ingrese una opcion valida! " << endl;
+                    cout << endl;
+                    regresar = 1;
+                    contador = 0;
+                    break;
                     }
                 } while(regresar == 0);
                 break; 
