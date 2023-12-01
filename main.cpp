5367096ea3f496e3218cc556d9c1231dfa73076c
Valency012 <00090123@uca.edu.sv>
2023-11-30 21:20:57 -0600

    Agregando Declaracion de variables y estructuras

--- main.cpp
+++ main.cpp
@@ -8,8 +8,27 @@
 using namespace std;
 
 
+//Menu principal
 int main()
 {
-    //Menu principal
+    // Declaracion de variables y estructuras
+
+    Bebida bebida;           // Declaracion estructura
+    Usuarios usuarios;       // Declaracion estructura
+    Ventas ventas;           // Declaracion estructura
+    int opcion;              // Variable encargada de evaluar la opcion ingresada por el cliente
+    string nombre_usuario;   // Variable que captura el nombre de usuario
+    string contrasenia;      // Variable que captura la clave
+    int tipo_usuario;        // Variable encargada de capturar el tipo de usuario
+    int sesion = 0;          // Variable que se encarga de cerrar sesion o mantenerla
+    int contador = 0;        // Variable la navegabilidad del sistema
+    int regresar = 0;        // Variable la navegabilidad del sistema
+    int regresarmenupro = 0; // Variable la navegabilidad del sistema en el menu productos
+    int regresarmenuusu = 0; // Variable la navegabilidad del sistema en el menu usuarios
+    int regresarmenuven = 0; // Variable la navegabilidad del sistema en el menu ventas
+    int opcion1;             // Variable encargada de evaluar la opcion ingresada por el cliente
+    int lote = 0;            // Variable que sirve como identificador cada producto, generada automaticamente
+    int identificador = 0;   // Variable que sirve como identificador unico cada venta
+
     return 0;
 }
