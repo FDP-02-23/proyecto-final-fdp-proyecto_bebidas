1588ffb44287cf61a43fc5cee44c31d083cd83d1
Romero39 <128502097+Romero39@users.noreply.github.com>
2023-12-01 01:44:33 -0600

    Funciones bitacora imprimir e agregar

--- funciones.cpp
+++ funciones.cpp
@@ -133,4 +133,196 @@ float BuscarPrecioVenta(int lote)
     }
     return contadorventa;
 }
+void AgregarBitacoraUsu(Bitacora_usu nuevabitacora)
+{
+    bitacora.push_back(nuevabitacora);
+}
+void AgregarBitacoraProd(Bitacora_prod nuevabitacora)
+{
+    bitacoraprod.push_back(nuevabitacora);
+}
+void AgregarBitacoraVen(Bitacora_ven nuevabitacora)
+{
+    bitacoraven.push_back(nuevabitacora);
+}
+void ImprimirBitacoraUsu(string nombre)
+{
+    // for off
+    for (Bitacora_usu bitacora : bitacora)
+    {
+        if (nombre == bitacora.usuario)
+        {
+            cout << endl;
+            cout << "Fecha de realizacion: " << bitacora.fecha << endl << "La accion realizada fue: " << bitacora.accion << endl;
+            if (bitacora.accion == "Se agrego un usuario")
+            {
+                if (bitacora.usuario_afectado == bitacora.usuarios.usuario)
+                {
+                    cout << "*******************Detalles**********************" << endl;
+                    cout << "El usuario ingresado fue el: " << bitacora.usuarios.usuario << endl;
+                    if (bitacora.usuarios.tipo_usuario == 1)
+                    {
+                        cout << "El tipo de usuario ingresado es jefe administrador" << endl;
+                    } else
+                    {
+                        cout << "El tipo de usuario ingresado es empleado administrador" << endl;
+                    }
+                        
+                }
+            } else if (bitacora.accion == "Se modifico un usuario")
+            {
+                
+                if (bitacora.usuario_afectado == bitacora.usuarios.usuario)
+                {
+                    cout << "*******************Detalles**********************" << endl;
+                    cout << "El usuario modificado fue: " << bitacora.usuarios.usuario << endl;
+                    if (bitacora.usuarios.tipo_usuario == 1)
+                    {
+                        cout << "El tipo de usuario es jefe administrador" << endl;
+                    } else
+                    {
+                        cout << "El tipo de usuario es empleado administrador" << endl;
+                    }
+                        
+                }
+            } else
+            {
+                if (bitacora.usuario_afectado == bitacora.usuarios.usuario)
+                {
+                    cout << "*******************Detalles**********************" << endl;
+                    cout << "El usuario eliminado fue: " << bitacora.usuarios.usuario << endl;
+                    if (bitacora.usuarios.tipo_usuario == 1)
+                    {
+                        cout << "El tipo de usuario era jefe administrador" << endl;
+                    } else
+                    {
+                        cout << "El tipo de usuario era empleado administrador" << endl;
+                    }
+                        
+                }
+            }   
+        }
+    }
+    cout << "\n\n" << endl;
+}
+void ImprimirBitacoraPro(string nombre)
+{
+    // for off
+    for (Bitacora_prod bitacora : bitacoraprod)
+    {
+        if (nombre == bitacora.usuario)
+        {
+            cout << endl;
+            cout << "Fecha de realizacion: " << bitacora.fecha << endl << "La accion realizada fue: " << bitacora.accion << endl;
+            if (bitacora.accion == "Se agrego un producto")
+            {
+                if (bitacora.producto_afectado == bitacora.bebida.nombre_bebida)
+                {
+                    cout << "*******************Detalles**********************" << endl;
+                    cout << "El producto ingresado fue el: " << bitacora.producto_afectado << endl;
+                    cout << "El numero de lote correspondiente es: " << bitacora.bebida.lote << endl;
+                    cout << "La cantidad adquirida fue de: " << bitacora.bebida.cantidad << endl;
+                    cout << "El precio de venta sera de: " << bitacora.bebida.precio_venta << endl;
+                    if (bitacora.bebida.tipo_bebida == 1)
+                    {
+                        cout << "El tipo de pack es de 6 botellas" << endl;
+                    } else if (bitacora.bebida.tipo_bebida == 2)
+                    {
+                        cout << "El tipo de pack es de 12 botellas" << endl;
+                    } else if (bitacora.bebida.tipo_bebida == 3)
+                    {
+                        cout << "El tipo de pack es de 24 botellas" << endl;
+                    }
+                        
+                }
+            } else if (bitacora.accion == "Se modifico un producto")
+            {
+                
+                if (bitacora.producto_afectado == bitacora.bebida.nombre_bebida)
+                {
+                    cout << "*******************Detalles**********************" << endl;
+                    cout << "El producto modificado fue el: " << bitacora.producto_afectado << endl;
+                    cout << "El numero de lote correspondiente es: " << bitacora.bebida.lote << endl;
+                    cout << "La cantidad adquirida fue de: " << bitacora.bebida.cantidad << endl;
+                    cout << "El precio de venta sera de: " << bitacora.bebida.precio_venta << endl;
+                    if (bitacora.bebida.tipo_bebida == 1)
+                    {
+                        cout << "El tipo de pack es de 6 botellas" << endl;
+                    } else if (bitacora.bebida.tipo_bebida == 2)
+                    {
+                        cout << "El tipo de pack es de 12 botellas" << endl;
+                    } else if (bitacora.bebida.tipo_bebida == 3)
+                    {
+                        cout << "El tipo de pack es de 24 botellas" << endl;
+                    }
+                        
+                }
+            } else
+            {
+                if (bitacora.producto_afectado == bitacora.bebida.nombre_bebida)
+                {
+                    cout << "*******************Detalles**********************" << endl;
+                    cout << "El producto eliminado fue el: " << bitacora.producto_afectado << endl;
+                    cout << "El numero de lote correspondiente fue: " << bitacora.bebida.lote << endl;
+                    cout << "La cantidad adquirida fue de: " << bitacora.bebida.cantidad << endl;
+                    cout << "El precio de venta fue de: " << bitacora.bebida.precio_venta << endl;
+                    if (bitacora.bebida.tipo_bebida == 1)
+                    {
+                        cout << "El tipo de pack es de 6 botellas" << endl;
+                    } else if (bitacora.bebida.tipo_bebida == 2)
+                    {
+                        cout << "El tipo de pack es de 12 botellas" << endl;
+                    } else if (bitacora.bebida.tipo_bebida == 3)
+                    {
+                        cout << "El tipo de pack es de 24 botellas" << endl;
+                    }
+                        
+                }
+            }   
+        }
+    }
+    cout << "\n\n" << endl;
+}
+void ImprimirBitacoraVen(string nombre)
+{
+    // for off
+    for (Bitacora_ven bitacora : bitacoraven)
+    {
+        if (nombre == bitacora.usuario)
+        {
+            cout << endl;
+            cout << "Fecha de realizacion: " << bitacora.fecha << endl << "La accion realizada fue: " << bitacora.accion << endl;
+            if (bitacora.accion == "Se agrego una venta")
+            {
+                if (bitacora.venta_afectada == bitacora.ventas.nombre_bebida)
+                {
+                    cout << "*******************Detalles**********************" << endl;
+                    cout << "El numero de venta es: $" << bitacora.ventas.identificador << endl;
+                    cout << "El nombre del consumidor es: $" << bitacora.ventas.nombre_consumidor << endl;
+                    cout << "El producto vendido fue el: " << bitacora.venta_afectada << endl;
+                    cout << "El numero de lote utilizado fue: " << bitacora.ventas.lote << endl;
+                    cout << "La cantidad vendida fue de: " << bitacora.ventas.cantidad << endl;
+                    cout << "El precio final fue de: $" << bitacora.ventas.precio_total << endl;
+                    
+                        
+                }
+            }  else
+            {
+                if (bitacora.venta_afectada == bitacora.ventas.nombre_bebida)
+                {
+                    cout << "*******************Detalles**********************" << endl;
+                    cout << "El numero de venta fue: $" << bitacora.ventas.identificador << endl;
+                    cout << "El nombre del consumidor fue: $" << bitacora.ventas.nombre_consumidor << endl;
+                    cout << "El producto vendido fue el: " << bitacora.venta_afectada << endl;
+                    cout << "El numero de lote utilizado fue: " << bitacora.ventas.lote << endl;
+                    cout << "La cantidad vendida fue de: " << bitacora.ventas.cantidad << endl;
+                    cout << "El precio final fue de: $" << bitacora.ventas.precio_total << endl;
+                        
+                }
+            }   
+        }
+    }
+    cout << "\n\n" << endl;
+}
+
 
