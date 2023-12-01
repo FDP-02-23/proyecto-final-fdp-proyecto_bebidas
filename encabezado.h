93622bd1a28be1af348dbb56d71b0429ae7f20c1
Romero39 <128502097+Romero39@users.noreply.github.com>
2023-12-01 01:11:26 -0600

    Declaracion de funciones en el archivo encabezado, ventas y bitacora

--- encabezado.h
+++ encabezado.h
@@ -9,7 +9,85 @@
 #include <string>
 
 using namespace std;
+//Declaraciones de estructuras a utilizar
+struct Usuarios
+{
+    string usuario;
+    string contrasenia;
+    int tipo_usuario;
+    string usuario_creacion;
+};
+struct Bebida
+{
+    string nombre_bebida;
+    int tipo_bebida;
+    float precio;
+    int cantidad;
+    float precio_venta;
+    int lote;
+};
+struct Ventas
+{
+    string nombre_bebida;
+    int lote;
+    int cantidad;
+    string nombre_consumidor;
+    float precio_total;
+    int identificador;
+    string fecha;
+};
+struct Bitacora_usu{
+    string usuario;
+    string accion;
+    string fecha;
+    string usuario_afectado;
+    Usuarios usuarios;
+};
+struct Bitacora_prod{
+    string usuario;
+    string accion;
+    string fecha;
+    string producto_afectado;
+    Bebida bebida;
+};
+struct Bitacora_ven{
+    string usuario;
+    string accion;
+    string fecha;
+    string venta_afectada;
+    Ventas ventas;
+};
 
+
+void InicializarUsuarios();
+int BuscarUsuario();
+int BuscarUsuario(string, string);
+void AgregarUsuarios(Usuarios);
+void ModificarUsuario(string);
+void ImprimirUsuarios();
+void EliminarUsuario(string);
+int BuscarUsuario(string, string);
+void ImprimirBebidas();
+void BuscaryModificarBebida(string, int);
+void ImprimirLotes(string);
+int BuscarBebidaPack(int, string);
+int BuscarBebidapack6(string);
+int BuscarBebidapack12(string);
+int BuscarBebidapack24(string);
+void EliminarBebida(string, int);
+void AgregarBebida(Bebida);
+int BuscarLote(int);
+string GetCurrentDate();
+void RegistrarVenta(Ventas venta);
+void AgregarVenta(Ventas);
+void EliminarVenta(string, int);
+void MostrarRegistro(string archivo); 
+void AgregarBitacoraUsu(Bitacora_usu); 
+void AgregarBitacoraProd(Bitacora_prod);
+void AgregarBitacoraVen(Bitacora_ven);
+void ImprimirBitacoraUsu(string);
+void ImprimirBitacoraPro(string);
+void ImprimirBitacoraVen(string);
 
 
 #endif // FUNCIONES_H
